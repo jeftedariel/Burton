3e69fcf8e2f9c17a7633d211e55e95c1a8fb62b0
jeftedariel <jeftedariel3@gmail.com>
2024-11-03 23:24:08 -0600

    init

---
+++ src/main/java/edu/utn/burton/Burton.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package edu.utn.burton;
+
+/**
+ *
+ * @author jefte
+ */
+public class Burton {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
