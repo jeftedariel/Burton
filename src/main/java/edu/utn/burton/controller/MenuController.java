7c353f9811a64aa594ec692222369458e24d2b92
jeftedariel <jeftedariel3@gmail.com>
2024-11-06 22:49:52 -0600

    menu, products & category created, some features added to menu

---
+++ src/main/java/edu/utn/burton/controller/MenuController.java
@@ -0,0 +1,64 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/javafx/FXMLController.java to edit this template
+ */
+package edu.utn.burton.controller;
+
+import edu.utn.burton.Burton;
+import edu.utn.burton.entities.Product;
+import edu.utn.burton.entities.ProductCell;
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.ListView;
+import javafx.stage.Stage;
+
+/**
+ * FXML Controller class
+ *
+ * @author jefte
+ */
+public class MenuController implements Initializable {
+
+    /**
+     * Initializes the controller class.
+     */
+    
+    
+    @FXML
+    private ListView<Product> productListView;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+        List<Product> products = List.of(
+            new Product(1, "Laptop", 799.99, "Laptop de 15 pulgadas", new String[]{"https://m.media-amazon.com/images/I/51dbotJw3XL.jpg"}, null),
+            new Product(2, "Smartphone", 499.99, "Smartphone con cámara de 12 MP", new String[]{"https://i.imgur.com/1twoaDy.jpeg"}, null),
+            new Product(3, "Tablet", 299.99, "Tablet con pantalla de 10 pulgadas", new String[]{"https://i.imgur.com/1twoaDy.jpeg"}, null)
+        );
+
+        // Configurar ListView y asignar productos
+        productListView.getItems().addAll(products);
+        productListView.setCellFactory(param -> new ProductCell());
+    }   
+    
+    public static void initGui() {
+        try {
+            Parent root = FXMLLoader.load(Burton.class.getResource("/fxml/Menu.fxml"));
+
+            Scene scene = new Scene(root);
+            Stage stage = new Stage();
+            stage.setTitle("Trackademia");
+            stage.setScene(scene);
+            stage.centerOnScreen();
+            stage.show();
+        } catch(IOException e){
+        }
+    }
+    
+}
---
+++ src/main/java/edu/utn/burton/entities/ProductCell.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package edu.utn.burton.entities;
+
+import javafx.scene.image.Image;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListCell;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+
+/**
+ *
+ * @author jefte
+ */
+public class ProductCell extends ListCell<Product> {
+        private ImageView imageView = new ImageView();
+        private Label titleLabel = new Label();
+        private Label priceLabel = new Label();
+        private Label descriptionLabel = new Label();
+
+        public ProductCell() {
+            imageView.setFitWidth(100);
+            imageView.setFitHeight(100);
+            setGraphic(new VBox(imageView, titleLabel, priceLabel, descriptionLabel));
+        }
+
+        @Override
+        protected void updateItem(Product product, boolean empty) {
+            super.updateItem(product, empty);
+            if (empty || product == null) {
+                setGraphic(null);
+            } else {
+                titleLabel.setText(product.title());
+                priceLabel.setText("$" + product.price());
+                descriptionLabel.setText(product.description());
+                imageView.setImage(new Image(product.images()[0])); // Muestra la primera imagen
+                setGraphic(new VBox(imageView, titleLabel, priceLabel, descriptionLabel));
+            }
+        }
+    }
