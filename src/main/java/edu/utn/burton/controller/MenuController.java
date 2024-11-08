89b39fdd5ee3227c53b6fc81835cdc333cacea2a
jeftedariel <jeftedariel3@gmail.com>
2024-11-08 13:57:58 -0600

    Filters for categories

--- src/main/java/edu/utn/burton/controller/MenuController.java
+++ src/main/java/edu/utn/burton/controller/MenuController.java
@@ -5,24 +5,28 @@
 package edu.utn.burton.controller;
 
 import edu.utn.burton.Burton;
+import edu.utn.burton.entities.Category;
 import edu.utn.burton.entities.Product;
 import edu.utn.burton.entities.ProductCell;
 import edu.utn.burton.handlers.APIHandler;
+import io.github.palexdev.materialfx.controls.MFXButton;
 import io.github.palexdev.materialfx.controls.legacy.MFXLegacyListView;
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
+import java.util.stream.Collectors;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Pos;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.Pagination;
 import javafx.scene.image.Image;
 import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 /**
@@ -41,28 +45,52 @@ public class MenuController implements Initializable {
     @FXML
     private Pagination pagination;
 
+    @FXML
+    private ComboBox cbxCategories;
+
+    @FXML
+    private MFXButton clearFilters;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
 
         loadProducts();
+        setCbxCategories();
+
         pagination.currentPageIndexProperty().addListener((obs, oldIndex, newIndex) -> {
             loadProducts();
         });
 
+        cbxCategories.setOnAction(ev -> {
+            loadProducts();
+        });
+
+        clearFilters.setOnAction(ev -> {
+            clearFilters();
+        });
     }
 
     public void loadProducts() {
         APIHandler api = new APIHandler(Product.class);
         List<Product> products = null;
-        try {
+        String categoryQuery = "";
+        
+        if (cbxCategories.getSelectionModel().getSelectedIndex() != -1) {
+            categoryQuery = "&categoryId=" + retrieveCategoryId();
+        }
 
-            products = api.obtenerProductos("products?offset=" + pagination.getCurrentPageIndex() * 10 + "&limit=10");
+        try {
+            products = api.obtenerProductos("products?offset=" + pagination.getCurrentPageIndex() * 10 + "&limit=10" + categoryQuery);
         } catch (Exception e) {
             System.out.println("Error: " + e);
         }
         productListView.getItems().clear();
         HBox row = new HBox(10);
         row.setAlignment(Pos.CENTER);
+        
+        //Set the corresponding size for pagination
+        //paginatio
+        
         //Iterates the products to put only 4 in it
         for (int i = 0; i < products.size(); i++) {
             ProductCell cell = new ProductCell();
@@ -78,7 +106,43 @@ public class MenuController implements Initializable {
         }
     }
 
+    public List<Category> loadCategories() {
+        APIHandler api = new APIHandler(Product.class);
+        List<Category> categories = null;
+        try {
+            categories = api.getCategories("categories");
+        } catch (Exception e) {
+            System.out.println("Error: " + e);
+        }
+        return categories;
+    }
+
+    public void setCbxCategories() {
+        List<String> categoryNames = loadCategories().stream()
+                .filter(cn -> !cn.name().isBlank())
+                .map(Category::name)
+                .distinct()
+                .collect(Collectors.toList());
+        pagination.setCurrentPageIndex(0);
+        cbxCategories.setItems(FXCollections.observableArrayList(categoryNames));
+    }
+
+    public int retrieveCategoryId() {
+        return loadCategories()
+                .stream()
+                .filter(n -> n.name().equals(cbxCategories.getItems().get(cbxCategories.getSelectionModel().getSelectedIndex()).toString()))
+                .distinct()
+                .toList()
+                .get(0).id();
+    }
+
+    public void clearFilters() {
+        pagination.setCurrentPageIndex(0);
+        cbxCategories.getSelectionModel().select(-1);
+    }
+
     public static void initGui() {
+
         try {
             Parent root = FXMLLoader.load(Burton.class.getResource("/fxml/Menu.fxml"));
 
@@ -88,7 +152,7 @@ public class MenuController implements Initializable {
             stage.setResizable(false);
             stage.getIcons().add(new Image(Burton.class.getResourceAsStream("/assets/icon.png")));
             scene.getStylesheets().add(Burton.class.getResource("/styles/menu.css").toExternalForm());
-
+            
             stage.setScene(scene);
             stage.centerOnScreen();
             stage.show();
