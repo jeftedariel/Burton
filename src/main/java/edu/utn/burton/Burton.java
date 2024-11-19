0286ce1f93c2e99896d356def2c5172354a9d257
jeftedariel <jeftedariel3@gmail.com>
2024-11-19 04:33:07 -0600

    fixing launch app

--- src/main/java/edu/utn/burton/Burton.java
+++ src/main/java/edu/utn/burton/Burton.java
@@ -4,7 +4,7 @@
 
 package edu.utn.burton;
 
-import edu.utn.burton.reports.SimpleReport;
+import edu.utn.burton.controller.LoginController;
 import javafx.application.Application;
 import javafx.stage.Stage;
 
@@ -13,13 +13,10 @@ import javafx.stage.Stage;
  * @author jefte
  */
 public class Burton extends Application {
-    
-    //666
+
     @Override
     public void start(Stage stage) throws Exception{
-        //LoginController.initGui();
-        SimpleReport sp = new SimpleReport();
-        sp.generate();
+        LoginController.initGui();
     }
     public static void main(String[] args) {
         launch(args);
