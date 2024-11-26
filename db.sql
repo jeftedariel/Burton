288251366290dab5c1d752f0c0def20fcd247157
jeftedariel <jeftedariel3@gmail.com>
2024-11-26 09:57:47 -0600

    fix db script

--- db.sql
+++ db.sql
@@ -89,7 +89,7 @@ CREATE TABLE `categories` (
   `id` int NOT NULL AUTO_INCREMENT,
   `name` varchar(255) NOT NULL,
   `image` text NOT NULL,
-  PRIMARY KEY (`category_id`)
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
